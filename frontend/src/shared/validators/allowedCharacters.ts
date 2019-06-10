de1386ceb2f09806d34e8dd64a20f511d2d55684
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-06-10 12:08:10 +0300

    Changed validation for dao name

--- frontend/src/shared/validators/allowedCharacters.ts
+++ frontend/src/shared/validators/allowedCharacters.ts
@@ -1,7 +1,7 @@
 import { tKeys, ITranslateKey } from 'services/i18n';
 
 export function allowedCharactersForDaoName(value: string): ITranslateKey | undefined {
-  const validationRegExp = new RegExp(`^(\\w)+$`);
+  const validationRegExp = new RegExp(`^[a-zA-Z\\d]+$`);
   return value && !validationRegExp.test(value)
     ? tKeys.shared.validation.allowedCharactersForDaoName.getKey()
     : undefined;
