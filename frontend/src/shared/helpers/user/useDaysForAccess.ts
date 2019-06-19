e1c976b5db431cdea6572b9f05ed39a079229b08
Burize <varimartas@yandex.ru>
2019-06-19 16:21:26 +0700

    fix discussions

--- frontend/src/shared/helpers/user/useDaysForAccess.ts
+++ frontend/src/shared/helpers/user/useDaysForAccess.ts
@@ -1,8 +1,13 @@
+import * as React from 'react';
+
 import { DaoApi } from 'services/daoApi';
 import useLastConfirmedJoinVoting from '../useLastConfirmedJoinVoting';
 import getWaitingAccessDays from '../getWaitingAccessDays';
 
 export default function useDaysForAccess(daoApi: DaoApi) {
   const lastJoinTransaction = useLastConfirmedJoinVoting(daoApi);
-  return lastJoinTransaction && getWaitingAccessDays(lastJoinTransaction.startDate);
+  return lastJoinTransaction && React.useMemo(
+    () => getWaitingAccessDays(lastJoinTransaction.startDate),
+    [lastJoinTransaction.startDate],
+  );
 }
