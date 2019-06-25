14c371128e33dc0015b8b01800ccd950c181d5a7
Dmitry Hursevich <Varimartas@yandex.ru>
2019-06-25 12:20:37 +0700

    add tokenMints to TokenManagerStore (#40)
    * add tokenMints to TokenManagerStore
    
    * remove condition from hook at useDaysForAccess

--- frontend/src/shared/helpers/user/index.ts
+++ frontend/src/shared/helpers/user/index.ts
@@ -1,2 +1,3 @@
 export { default as useUserBalance } from './useUserBalance';
 export { default as useDaysForAccess } from './useDaysForAccess';
+export { default as useIsUserMember } from './useIsUserMember';
