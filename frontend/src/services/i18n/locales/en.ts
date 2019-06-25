f000cbef60ce649e5e74d712d095d8adc1f388d7
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-06-25 13:13:13 +0300

    Fixed typo

--- frontend/src/services/i18n/locales/en.ts
+++ frontend/src/services/i18n/locales/en.ts
@@ -26,7 +26,7 @@ export default {
     copiedAtClipboard: 'Copied at clipboard',
     cancel: 'Cancel',
     retry: 'Retry',
-    noEthereumConnection: 'No connection to the Etherium network',
+    noEthereumConnection: 'No connection to the Ethereum network',
     noEthereumAccounts: 'We can\'t find any Ethereum accounts! Please check and make sure Metamask or your browser are pointed at the correct network and your account is unlocked.',
     needUseMetamask: 'This browser has no connection to the Ethereum network. Please use the Chrome/FireFox extension MetaMask, or dedicated Ethereum browsers Mist or Parity.',
     makeSureUseKovan: 'Please make sure you are using Rinkeby Test Network when connecting your Metamask',
